cd93baa696ece05a44ea79da27e5df3179612ad3
asayed <ahmedsayed.9191@gmail.com>
2020-10-30 21:46:48 +0200

    Implement no items scenario

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt,faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 import './ItemList.css';
 
 const ItemList =(props) =>{
     const {items,deleteItem} = props;
-    const deleteIcon = <FontAwesomeIcon icon={faTrashAlt} />
-    const ItemList = items.map(item => {
-        return(
-            <div key={item.id}>
-                <span>{item.task}</span>
-                <span>{item.priority}</span>
-                <span onClick={() => deleteItem(item.id)}>{deleteIcon}</span>
-            </div>
-        )
-    })
+    const deleteIcon = <FontAwesomeIcon icon={faTrashAlt} />  
+    const exclamationIcon = <FontAwesomeIcon icon={faExclamationTriangle} />
+    let length = items.length;
+    const ItemList = length ?(
+        items.map(item => {
+            return(
+                <div key={item.id}>
+                    <span>{item.task}</span>
+                    <span>{item.priority}</span>
+                    <span onClick={() => deleteItem(item.id)}>{deleteIcon}</span>
+                </div>
+            )
+        })
+    ):(<p>{exclamationIcon}You don't have items! start adding</p>)
     return(
         <div className="ListItems">
             <span>Task</span>
