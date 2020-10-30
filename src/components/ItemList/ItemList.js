c2f34860aa72d02c299dcbb5ef3ba082476a2029
asayed <ahmedsayed.9191@gmail.com>
2020-10-30 21:02:44 +0200

    Add font awesome to the app

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import './ItemList.css';
 
 const ItemList =(props) =>{
     const {items} = props;
+    const deleteIcon = <FontAwesomeIcon icon={faTrashAlt} />
     const ItemList = items.map(item => {
         return(
             <div key={item.id}>
                 <span>{item.task}</span>
                 <span>{item.priority}</span>
-                <span>&times;</span>
+                <span>{deleteIcon}</span>
             </div>
         )
     })
